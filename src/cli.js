88b5e43397572945872ea93ac0fc45d0943994fd
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2022-11-19 16:03:50 +0700

    Fix linter errors and warnings

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export function greeting() {
-    console.log("Welcome to the Brain Games!");
+function greeting() {
+  console.log('Welcome to the Brain Games!');
 
-    const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
 
-    console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
 }
+
+export default greeting();
