4f8e1de5231c28b0581ed7dfeae1d7a612d1f25a
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2022-11-21 23:59:52 +0700

    Add game 'brain-gcd'

---
+++ src/games/gcd.js
@@ -0,0 +1,35 @@
+import playGame from '../index.js';
+
+const NUMBER_OF_ROUNDS = 3;
+
+const findGcd = (a, b) => {
+  const minNumber = Math.min(a, b);
+  let result = 1;
+
+  for (let i = 2; i <= minNumber; i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      result = i;
+    }
+  }
+
+  return result;
+};
+
+export default function gcd() {
+  const gameRules = 'Find the greatest common divisor of given numbers.';
+  const gameRounds = [];
+
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const number1 = Math.round(Math.random() * 100 + 1);
+    const number2 = Math.round(Math.random() * 100 + 1);
+
+    const rightAnswer = findGcd(number1, number2);
+
+    const question = (`Question : ${number1} ${number2}`);
+    const gameRound = [question, rightAnswer.toString()];
+
+    gameRounds.push(gameRound);
+  }
+
+  playGame(gameRules, gameRounds);
+}
