860627b7b4611b5338c7cf50407962de8a282bd0
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2022-11-22 22:10:49 +0700

    Add game 'brain-prime'

---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+import playGame from '../index.js';
+
+const NUMBER_OF_ROUNDS = 3;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default function prime() {
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const gameRounds = [];
+
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const randomNumber = Math.round(Math.random() * 100 + 1);
+
+    const question = (`Question : ${randomNumber}`);
+    const rightAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+    const gameRound = [question, rightAnswer];
+    gameRounds.push(gameRound);
+  }
+
+  playGame(gameRules, gameRounds);
+}
